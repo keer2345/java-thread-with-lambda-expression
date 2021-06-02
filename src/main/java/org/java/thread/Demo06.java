06145475f3975ba4cc08bbab7e7f80ba4fd3b001
keer2345 <keer2345@gmail.com>
2021-06-02 10:18:26 +0800

    Update Demo06.java

--- src/main/java/org/java/thread/Demo06.java
+++ src/main/java/org/java/thread/Demo06.java
@@ -1,37 +1,37 @@
 package org.java.thread;
 
-class Counter {
-  int count;
-
-  public void increment() {
-    count++;
-  }
-}
 
 public class Demo06 {
+  private static int count = 0;
+
+  private static synchronized void countInc(){
+      count++;
+  }
+
   public static void main(String[] args) throws InterruptedException {
-    Counter c = new Counter();
     Thread t1 =
         new Thread(
             () -> {
-              for (int i = 1; i <= 1000; i++) {
-                c.increment();
+              for (int i = 0; i < 100000; i++) {
+                  countInc();
               }
             });
     Thread t2 =
         new Thread(
             () -> {
-              for (int i = 1; i <= 1000; i++) {
-                c.increment();
+              for (int i = 0; i < 100000; i++) {
+                  countInc();
               }
             });
 
     t1.start();
     t2.start();
 
+    // Java thread join method can be used to pause the current thread execution
+    // until unless the specified thread is dead
     t1.join();
     t2.join();
 
-    System.out.println("Count " + c.count);
+    System.out.println("Value: " + count);
   }
 }
